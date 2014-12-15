cdb7056865b5cbfca17e6c483246ef69852cef59
Thumadon <imageandauthority@gmail.com>
2014-12-14 23:08:56 -0800

    card 12 adjusted widget size. sound works

--- app/controllers/ekkos_controller.rb
+++ app/controllers/ekkos_controller.rb
@@ -5,8 +5,6 @@ class EkkosController < ApplicationController
   def index
     user_hash = @user_client.get('/me')
     @username = user_hash.username
-
     @user_tracks = @user_client.get("/me/tracks")
-
   end
 end
