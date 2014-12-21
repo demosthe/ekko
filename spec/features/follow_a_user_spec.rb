d28f338f77541ae57987b20333edebcb0234f73c
Thumadon <imageandauthority@gmail.com>
2014-12-20 22:37:18 -0800

    user cannot follow himself

--- spec/features/follow_a_user_spec.rb
+++ spec/features/follow_a_user_spec.rb
@@ -1,14 +1,22 @@
 require "rails_helper"
 
-feature "User can follow another user" do
-  scenario "successfully" do
+feature "User tries follow an user" do
+  scenario "can follow another user succesfully" do
     user1 = create :user
     user2 = create :user
 
     login_as user1
-    visit user_path user2
+    visit user_path user1
     click_link "Follow"
 
     expect(page).to have_content("You are now following #{user.username}")
+  end
+
+  scenario "cannot follow user himself" do
+    user1 = create :user
+
+    login_as user1
+    visit user_path user1
+    expect(page).to_not have_content("Follow")
   end
 end
