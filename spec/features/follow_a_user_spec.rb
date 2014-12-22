ca6df7f7063395c6e44063d2a65f987bac47de87
Thumadon <imageandauthority@gmail.com>
2014-12-22 13:57:17 -0800

    card 18 cannot follow user that he is already following

--- spec/features/follow_a_user_spec.rb
+++ spec/features/follow_a_user_spec.rb
@@ -1,22 +1,37 @@
 require "rails_helper"
 
 feature "User tries follow an user" do
-  scenario "can follow another user succesfully" do
-    user1 = create :user
+
+  let(:user) { create :user }
+
+  scenario "can follow another user" do
     user2 = create :user
 
-    login_as user1
+    login_as user
     visit user_path user2
     click_button "Follow"
-
     expect(page).to have_content("You are now following #{user2.username}")
   end
 
-  scenario "cannot follow user himself" do
-    user1 = create :user
+  scenario "can unfollow another user" do
+    skip
+    user2 = create :user
+  end
 
-    login_as user1
-    visit user_path user1
+  scenario "cannot follow user himself" do
+    login_as user
+    visit user_path user
     expect(page).to_not have_content("Follow")
   end
+
+  scenario "cannot follow an user that he is already following" do
+    user2 = create :user
+    user.follow!(user2)
+
+    login_as user
+    visit user_path user2
+    expect(page).to_not have_content("Follow")
+  end
+
+
 end
