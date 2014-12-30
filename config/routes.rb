1e1f8887fe69eda0289e975382a165b29cc96ee1
Thumadon <imageandauthority@gmail.com>
2014-12-30 08:46:43 -0800

    card 20 configure bootstrap. added topbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "ekkos#index"
   devise_for :users
 
+  resources :ekkos, only: [:index]
   resources :users, only: [:show]
   resources :relationships, only: [:create, :destroy]
 end
