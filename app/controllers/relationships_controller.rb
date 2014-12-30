f25ea2faa8b251032ae4be90f73ac412e9312219
Thumadon <imageandauthority@gmail.com>
2014-12-29 17:26:36 -0800

    card 18 unfollow user

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,4 +5,10 @@ class RelationshipsController < ApplicationController
     current_user.follow!(@user)
     redirect_to @user, notice: "You are now following #{@user.username}"
   end
+
+  def destroy
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.unfollow!(@user)
+    redirect_to @user, notice: "You just unfollowed #{@user.username}"
+  end
 end
--- spec/features/follow_a_user_spec.rb
+++ spec/features/follow_a_user_spec.rb
@@ -4,34 +4,43 @@ feature "User tries follow an user" do
 
   let(:user) { create :user }
 
+  before(:each) do
+    login_as user
+  end
+
   scenario "can follow another user" do
     user2 = create :user
 
-    login_as user
     visit user_path user2
     click_button "Follow"
     expect(page).to have_content("You are now following #{user2.username}")
   end
 
-  scenario "can unfollow another user" do
-    skip
-    user2 = create :user
+  context "already following user" do
+    let(:user2) { create :user }
+
+    before(:each) do
+      user.follow!(user2)
+    end
+
+    scenario "cannot follow an user that he is already following" do
+      visit user_path user2
+      expect(page).to_not have_content("Follow")
+    end
+
+    scenario "can unfollow another user" do
+      visit user_path user2
+      click_button "Unfollow"
+      expect(page).to have_content("You just unfollowed #{user2.username}")
+      expect(user.following?(user2)).to be_nil
+    end
   end
 
   scenario "cannot follow user himself" do
-    login_as user
     visit user_path user
     expect(page).to_not have_content("Follow")
   end
 
-  scenario "cannot follow an user that he is already following" do
-    user2 = create :user
-    user.follow!(user2)
-
-    login_as user
-    visit user_path user2
-    expect(page).to_not have_content("Follow")
-  end
 
 
 end
