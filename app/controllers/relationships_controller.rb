28e0984e44e9f9623bdac2eee3dcc0f7f26453d1
Thumadon <imageandauthority@gmail.com>
2015-01-07 19:14:36 -0800

    card 27 can follow soundcloud user

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,23 @@
 class RelationshipsController < ApplicationController
   #before_action :signed_in_user
   def create
-    @user = User.find(params[:relationship][:followed_id])
+    soundcloud_id = params["soundcloud_id"]
+
+    if soundcloud_id.nil?
+      followed_id = params[:relationship][:followed_id]
+      @user = User.find(followed_id)
+    else
+      user = User.find_or_initialize_by(soundcloud_id: soundcloud_id)
+      # todo: pass in username
+      user.username = params["username"]
+      user.password = ENV["default_user_password"]
+      user.email = "fakemail@gmail.con"
+      user.save!
+      @user = user
+    end
+
     current_user.follow!(@user)
+
     redirect_to @user, notice: "You are now following #{@user.username}"
   end
 
