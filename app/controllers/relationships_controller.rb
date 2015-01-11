1ab0b4b096ec1291afde63f0905815fc7198385e
Thumadon <imageandauthority@gmail.com>
2015-01-10 22:45:53 -0800

    card 27 follow/unfollow soundcloud user

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,13 @@
 class RelationshipsController < ApplicationController
   def create
-    followed_id = params[:relationship][:followed_id]
-    @user = User.find(followed_id)
+    @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-
-    redirect_to @user, notice: "You are now following #{@user.username}"
+    redirect_to :back, notice: "You are now following #{@user.username}"
   end
 
   def destroy
     @user = User.find(params[:relationship][:followed_id])
     current_user.unfollow!(@user)
-    redirect_to @user, notice: "You just unfollowed #{@user.username}"
+    redirect_to :back, notice: "You just unfollowed #{@user.username}"
   end
 end
