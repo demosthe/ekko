2008c9d5f50e0c1223deded820e6b77946d5a803
Thumadon <imageandauthority@gmail.com>
2015-01-11 22:09:59 -0800

    card 32 stream only followed users

--- app/controllers/ekkos_controller.rb
+++ app/controllers/ekkos_controller.rb
@@ -5,19 +5,14 @@ class EkkosController < ApplicationController
   def index
     @ekkos = @user_client.get("/me/tracks")
 
-    # algorithm: for every user that I am following
-    #  get their ekkos
-    #  add their ekkos to @ekkos
-
-    user_hash = @client.get('/resolve',
-                            url: 'https://soundcloud.com/lords-recovery-cdo/')
-    user_tracks_hash =
-      @client.get("/users/#{user_hash['id']}/tracks")
-
-    @ekkos.concat(user_tracks_hash)
+    current_user.followed_users.each do |user|
+      user_hash = @client.get("/resolve",
+                              url: "https://soundcloud.com/#{user.username}/")
+      user_tracks_hash = @client.get("/users/#{user_hash['id']}/tracks")
+      @ekkos.concat(user_tracks_hash)
+    end
 
     @ekkos.sort! { |a,b| b['id'] <=> a['id'] }
-
     @ekkos = @ekkos.paginate(page: params[:page], per_page: 4)
   end
 end
