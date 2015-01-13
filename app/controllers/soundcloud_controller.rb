bcf3d747f12d548954352d238f73b45d11c0abe0
Thumadon <imageandauthority@gmail.com>
2015-01-13 11:23:48 -0800

    user permalink for ekko email

--- app/controllers/soundcloud_controller.rb
+++ app/controllers/soundcloud_controller.rb
@@ -11,7 +11,7 @@ class SoundcloudController < ApplicationController
     user = User.new(soundcloud_id: params[:soundcloud_id])
     user.username = params[:username]
     user.password = ENV["default_user_password"]
-    user.email = "#{params[:username]}@soundcloud.com"
+    user.email = "#{params[:permalink]}@soundcloud.com"
     user.save!
 
     redirect_to :back
